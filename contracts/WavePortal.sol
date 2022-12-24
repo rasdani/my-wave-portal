b34dca014db9af33031cb85ea3860cd77f264047
rasdani <mail.rasdani@protonmail.com>
2022-12-24 09:59:57 +0100

    added random payout and cooldown timer

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
   mapping(address => uint256) public waveBalances;
+  uint private seed;
 
   event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -18,27 +19,45 @@ contract WavePortal {
 
   Wave[] waves;
 
+  mapping(address => uint256) public lastWavedAt;
+
   constructor() payable {
     console.log("Contract constructed!");
+    console.log("Timestamp: ", block.timestamp);
+    console.log("Difficulty: ", block.difficulty);
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
   function wave(string memory _message) public {
+    require(
+      lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+      "Wait 30s"
+    );
+    lastWavedAt[msg.sender] = block.timestamp;
+
     waveBalances[msg.sender] += 1;
     totalWaves += 1;
     console.log("%s has waved: %s", msg.sender, _message);
 
     waves.push(Wave(msg.sender, _message, block.timestamp));
 
-    emit NewWave(msg.sender, block.timestamp, _message);
+    seed = (block.timestamp + block.difficulty) % 100;
+    console.log("Random seed generated: %d", seed);
 
-    uint256 prizeAmount = 0.0001 ether;
-    require(
-      prizeAmount <= address(this).balance,
-      "Trying to withdraw more money than the contract has."
-    );
+    if (seed < 50) {
+      console.log("%s won!", msg.sender);
 
-    (bool succes, ) = (msg.sender).call{value: prizeAmount}("");
-    require(succes, "Failed to withdraw money from contract.");
+      uint256 prizeAmount = 0.0001 ether;
+      require(
+        prizeAmount <= address(this).balance,
+        "Trying to withdraw more money than the contract has."
+      );
+
+      (bool succes, ) = (msg.sender).call{value: prizeAmount}("");
+      require(succes, "Failed to withdraw money from contract.");
+    }
+
+      emit NewWave(msg.sender, block.timestamp, _message);
   }
 
   function getAllWaves() public view returns (Wave[] memory) {
